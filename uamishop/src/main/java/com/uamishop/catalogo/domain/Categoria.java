02b66904096e29a60c93fa7bb613127112a4227b
Vale_DarkonUwU <cbi2213042143@izt.uam.mx>
2026-02-09 19:36:06 +0000

    Datos subidos

--- uamishop/src/main/java/com/uamishop/catalogo/domain/Categoria.java
+++ uamishop/src/main/java/com/uamishop/catalogo/domain/Categoria.java
@@ -1,5 +1,16 @@
 package com.uamishop.catalogo.domain;
 
 public class Categoria {
+    private final CategoriaId id;
+    private String nombre;
+    private Categoria categoriaPadre;
 
+    public Categoria(CategoriaId id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public void asignarCategoriaPadre(Categoria padre) {
+        this.categoriaPadre = padre;
+    }
 }
