e7baa0ad4cab33f1c365e0b82a7246493d08feea
Vale_DarkonUwU <cbi2213042143@izt.uam.mx>
2026-02-20 02:36:59 +0000

    Actualice el repository de domain porque tenia un error de eescritura

--- uamishop/src/main/java/com/uamishop/ordenes/repository/OrdenJpaRepository.java
+++ uamishop/src/main/java/com/uamishop/ordenes/repository/OrdenJpaRepository.java
@@ -3,6 +3,6 @@ package com.uamishop.ordenes.repository;
 import com.uamishop.ordenes.domain.Orden;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interace OrdenRepository extends JpaRepository<Orden, UUID> {
+public interface OrdenRepository extends JpaRepository<Orden, UUID> {
     
 }
