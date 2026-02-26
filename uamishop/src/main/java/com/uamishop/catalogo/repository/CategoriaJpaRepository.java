ad4d3a02e2f26c5754a924c0d6c11c5ffbc71ff6
Vale_DarkonUwU <cbi2213042143@izt.uam.mx>
2026-02-26 21:26:55 +0000

    Se ajusto CategoriaService y CategoriaJpaRepository por la UUID que marcaba un error

--- uamishop/src/main/java/com/uamishop/catalogo/repository/CategoriaJpaRepository.java
+++ uamishop/src/main/java/com/uamishop/catalogo/repository/CategoriaJpaRepository.java
@@ -1,5 +1,6 @@
 package com.uamishop.catalogo.repository;
 import com.uamishop.catalogo.domain.Categoria;
+import com.uamishop.catalogo.domain.CategoriaId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
