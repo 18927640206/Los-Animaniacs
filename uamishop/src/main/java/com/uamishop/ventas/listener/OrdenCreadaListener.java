83552f0f3559203b18331a9f41378d669817820d
NanChavez <cbi2203042622@izt.uam.mx>
2026-03-07 21:23:48 +0000

    se creo la carpeta litener

---
+++ uamishop/src/main/java/com/uamishop/ventas/listener/OrdenCreadaListener.java
@@ -0,0 +1,22 @@
+package com.uamishop.ventas.listener;
+
+import com.uamishop.shared.event.OrdenCreadaEvent;
+import com.uamishop.ventas.domain.CarritoId; 
+import com.uamishop.ventas.service.CarritoService;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrdenCreadaListener {
+
+    private final CarritoService carritoService;
+
+    public OrdenCreadaListener(CarritoService carritoService) {
+        this.carritoService = carritoService;
+    }
+
+    @EventListener
+    public void handle(OrdenCreadaEvent event) {
+        carritoService.completarCheckout(CarritoId.of(event.carritoId()));
+    }
+}
